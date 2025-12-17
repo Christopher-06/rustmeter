e0664c7b5f94796fca7f3b888f13405e3bb572fe
Christopher Kock <christopher-kock@outlook.de>
2025-12-17 20:49:10 +0100

    Do cargo build instead of run

--- rustmeter-cli/src/cargo/cargo_build.rs
+++ rustmeter-cli/src/cargo/cargo_build.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 // {"reason":"compiler-artifact","package_id":"registry+https://github.com/rust-lang/crates.io-index#esp-rtos@0.2.0","manifest_path":"C:\\Users\\chris\\.cargo\\registry\\src\\index.crates.io-1949cf8c6b5b557f\\esp-rtos-0.2.0\\Cargo.toml","target":{"kind":["lib"],"crate_types":["lib"],"name":"esp_rtos","src_path":"C:\\Users\\chris\\.cargo\\registry\\src\\index.crates.io-1949cf8c6b5b557f\\esp-rtos-0.2.0\\src\\lib.rs","edition":"2024","doc":true,"doctest":true,"test":true},"profile":{"opt_level":"s","debuginfo":2,"debug_assertions":true,"overflow_checks":true,"test":false},"features":["default","defmt","embassy","esp32"],"filenames":["C:\\Users\\chris\\Documents\\Projekte\\embassy-tracer\\esp32-embassy-tracer\\target\\xtensa-esp32-none-elf\\debug\\deps\\libesp_rtos-0c0bbec6a5ad5299.rlib","C:\\Users\\chris\\Documents\\Projekte\\embassy-tracer\\esp32-embassy-tracer\\target\\xtensa-esp32-none-elf\\debug\\deps\\libesp_rtos-0c0bbec6a5ad5299.rmeta"],"executable":null,"fresh":true}
 // {"reason":"compiler-artifact","package_id":"path+file:///C:/Users/chris/Documents/Projekte/embassy-tracer/esp32-embassy-tracer#0.1.0","manifest_path":"C:\\Users\\chris\\Documents\\Projekte\\embassy-tracer\\esp32-embassy-tracer\\Cargo.toml","target":{"kind":["lib"],"crate_types":["lib"],"name":"esp32_embassy_tracer","src_path":"C:\\Users\\chris\\Documents\\Projekte\\embassy-tracer\\esp32-embassy-tracer\\src\\lib.rs","edition":"2024","doc":true,"doctest":true,"test":true},"profile":{"opt_level":"s","debuginfo":2,"debug_assertions":true,"overflow_checks":true,"test":false},"features":[],"filenames":["C:\\Users\\chris\\Documents\\Projekte\\embassy-tracer\\esp32-embassy-tracer\\target\\xtensa-esp32-none-elf\\debug\\libesp32_embassy_tracer.rlib","C:\\Users\\chris\\Documents\\Projekte\\embassy-tracer\\esp32-embassy-tracer\\target\\xtensa-esp32-none-elf\\debug\\deps\\libesp32_embassy_tracer-cc08d6f9ed74997c.rmeta"],"executable":null,"fresh":true}
@@ -29,67 +30,15 @@ impl CargoBuildMessage {
             Err(e) => Err(anyhow::anyhow!("Failed to parse cargo build message: {e}")),
         }
     }
-}
 
-#[derive(Debug, Clone, Serialize)]
-pub enum CargoBuildStatus {
-    /// Indicates that the build process has just
-    Started,
-    /// Indicates that the build process is currently running, with optional executable path if found
-    Running(Option<String>),
-    /// Indicates that the build process has completed successfully with the given executable path
-    Success(Option<String>),
-    /// Indicates that the build process has failed
-    Failed(String),
-}
-
-impl CargoBuildStatus {
-    pub fn has_finished(&self) -> bool {
-        matches!(
-            self,
-            CargoBuildStatus::Success(_) | CargoBuildStatus::Failed(_)
-        )
-    }
-
-    pub fn has_failed(&self) -> bool {
-        matches!(self, CargoBuildStatus::Failed(_))
-    }
-
-    pub fn try_get_executable(&self) -> &Option<String> {
-        match self {
-            CargoBuildStatus::Running(exe) => exe,
-            CargoBuildStatus::Success(exe) => exe,
-            _ => &None,
-        }
-    }
-
-    pub fn update_from_build_line(self, line: &str) -> Self {
-        if self.has_finished() {
-            return self; // already finished
-        }
-
-        // Running or Started
-        match CargoBuildMessage::from_build_line(line) {
-            Ok(message) => match message {
-                CargoBuildMessage::BuildFinished { success } => {
-                    if success {
-                        CargoBuildStatus::Success(self.try_get_executable().clone())
-                    } else {
-                        CargoBuildStatus::Failed("Build process reported failure".to_string())
-                    }
-                }
-                CargoBuildMessage::CompilerArtifact { executable, .. } => {
-                    CargoBuildStatus::Running(executable)
-                }
-                _ => self, // irgnore other messages
-            },
-            Err(_) => {
-                // Parse Error means normal build log line, print and return
-                if !line.trim().starts_with("{") {
-                    println!("{line}");
-                }
-                self
+    /// Returns the executable path if this is a CompilerArtifact message with an executable info
+    pub fn get_elf_path(&self) -> Option<PathBuf> {
+        if let CargoBuildMessage::CompilerArtifact { executable, .. } = self {
+            if let Some(path) = executable {
+                return Some(PathBuf::from(path));
             }
         }
+
+        None
     }
 }
--- rustmeter-cli/src/cargo/cargo_child.rs
+++ rustmeter-cli/src/cargo/cargo_child.rs
@@ -1,52 +1,47 @@
-use std::process::{Command, Stdio};
+use std::{
+    path::PathBuf,
+    process::{Command, Stdio},
+    sync::{Arc, Mutex},
+};
 
-use crate::cargo::cargo_build::CargoBuildStatus;
-use anyhow::Context;
-use crossbeam::channel::{Receiver, Sender};
+use crate::cargo::cargo_build::CargoBuildMessage;
+use anyhow::{Context, bail};
 
+/// Represents a child process running a Cargo Build command.
 pub struct CargoChildProcess {
+    /// Receiver for build status updates
     child: std::process::Child,
 
-    logs_recver: Receiver<String>,
-
-    build_status_recver: Receiver<CargoBuildStatus>,
-    current_build_status: CargoBuildStatus,
+    /// Path to the built executable (if available)
+    elf_path: Arc<Mutex<Option<PathBuf>>>,
 }
 
 impl CargoChildProcess {
-    pub fn kill(mut self) -> anyhow::Result<()> {
-        self.child.kill().context("Tried to kill child process")
-    }
-
-    pub fn get_status_code(&mut self) -> anyhow::Result<Option<std::process::ExitStatus>> {
-        self.child
-            .try_wait()
-            .context("Failed to try waiting on child process to gather exit status")
-    }
-
-    /// Synchronously waits for the build process to finish.
-    pub fn wait_build_finish(&mut self) -> anyhow::Result<CargoBuildStatus> {
-        // Wait for updates
-        while let Ok(val) = self.build_status_recver.recv() {
-            self.current_build_status = val;
-
-            if self.current_build_status.has_finished() {
-                return Ok(self.current_build_status.clone());
-            }
+    /// Synchronously waits for the process to finish and returns the path to the built executable.
+    pub fn wait_till_finished(&mut self) -> anyhow::Result<PathBuf> {
+        // wait till finished
+        let exit_status = self
+            .child
+            .wait()
+            .context("Failed to wait on child process")?;
+        if !exit_status.success() {
+            bail!("Child process exited with non-zero status: {}", exit_status);
         }
 
-        // Sender has been closed ==> unexpected error
-        Err(anyhow::anyhow!("Build status channel closed unexpectedly"))
+        // get executable path
+        let elf_path = match self.elf_path.lock() {
+            Ok(guard) => guard.clone(),
+            Err(_) => bail!("Failed to lock elf_path mutex"),
+        };
+
+        match elf_path {
+            Some(path) => Ok(path),
+            None => bail!("Executable path not found after process finished"),
+        }
     }
 
-    pub fn get_logs_receiver(&self) -> Receiver<String> {
-        self.logs_recver.clone()
-    }
-
-    pub fn new_start_run(release: bool, project_dir: &str) -> anyhow::Result<Self> {
-        let (build_status_sender, build_status_recver) = crossbeam::channel::unbounded();
-        let (logs_sender, logs_recver) = crossbeam::channel::unbounded();
-
+    /// Starts a new Cargo build process in the specified project directory with the given release flag.
+    pub fn new_start_build(release: bool, project_dir: &str) -> anyhow::Result<Self> {
         // Create Command
         let mut cmd = Command::new("cargo");
         cmd.stdout(Stdio::piped());
@@ -54,7 +49,7 @@ impl CargoChildProcess {
         cmd.current_dir(project_dir);
 
         // Add arguments
-        cmd.arg("run");
+        cmd.arg("build");
         cmd.arg("--message-format")
             .arg("json-diagnostic-rendered-ansi"); // for easier parsing of build output
         if release {
@@ -67,34 +62,22 @@ impl CargoChildProcess {
             .stdout
             .take()
             .context("Failed to take stdout of cargo process")?;
-        let _ = read_to_channel_threaded(stdout, build_status_sender, logs_sender);
 
-        Ok(CargoChildProcess {
-            child,
-            logs_recver,
-            build_status_recver,
-            current_build_status: CargoBuildStatus::Started,
-        })
-    }
-}
+        let elf_path = Arc::new(Mutex::new(None));
+        let _ = read_to_channel_threaded(stdout, elf_path.clone());
 
-impl Drop for CargoChildProcess {
-    fn drop(&mut self) {
-        let _ = self.child.kill();
+        Ok(CargoChildProcess { child, elf_path })
     }
 }
 
 /// Reads from the given reader and sends the output to the provided channel sender.
 fn read_to_channel_threaded<R: std::io::Read + Send + 'static>(
     mut reader: R,
-    build_status_sender: Sender<CargoBuildStatus>,
-    logs_sender: Sender<String>,
+    elf_path: Arc<Mutex<Option<PathBuf>>>,
 ) -> std::thread::JoinHandle<()> {
     std::thread::spawn(move || {
         let mut byte_buffer = [0; 1024];
         let mut string_buffer = String::new();
-
-        let mut last_build_status = CargoBuildStatus::Started;
 
         loop {
             // Read next bytes or exit
@@ -117,19 +100,23 @@ fn read_to_channel_threaded<R: std::io::Read + Send + 'static>(
             // Process complete lines
             while let Some(pos) = string_buffer.find('\n') {
                 let line = string_buffer.drain(..=pos).collect::<String>();
-                if !last_build_status.has_finished() {
-                    // Parse Build line to CargoBuildStatus
-                    last_build_status =
-                        CargoBuildStatus::update_from_build_line(last_build_status, &line);
-                    let ch_closed = build_status_sender.send(last_build_status.clone()).is_err();
+                let parse_res = CargoBuildMessage::from_build_line(line.trim());
 
-                    if ch_closed || last_build_status.has_failed() {
-                        return; // Stop processing if receiver is closed or build failed
+                match parse_res {
+                    Ok(message) => {
+                        // Check for elf path
+                        if let Some(elf_path_buf) = message.get_elf_path() {
+                            if let Ok(mut guard) = elf_path.lock() {
+                                // store elf path
+                                *guard = Some(elf_path_buf);
+                            }
+                        }
                     }
-                } else {
-                    // Log line
-                    if logs_sender.send(line).is_err() {
-                        return; // Stop processing if receiver is closed
+                    Err(_) => {
+                        // Just text output, print it
+                        if !line.trim().starts_with("{") {
+                            print!("{line}");
+                        }
                     }
                 }
             }
