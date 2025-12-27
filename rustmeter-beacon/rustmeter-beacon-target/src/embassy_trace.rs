90dfa9ad25a67bed13e590ac38638dbf0bc46044
Christopher Kock <christopher-kock@outlook.de>
2025-12-27 23:04:16 +0100

    Enter critical section in embassy events

--- rustmeter-beacon/rustmeter-beacon-target/src/embassy_trace.rs
+++ rustmeter-beacon/rustmeter-beacon-target/src/embassy_trace.rs
@@ -17,86 +17,100 @@ static EXECUTOR_REGISTRY: ExecutorRegistry = ExecutorRegistry::new();
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_poll_start(executor_id: u32) {
-    let payload = EventPayload::EmbassyExecutorPollStart {
-        executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-    };
+    critical_section::with(|_| {
+        let payload = EventPayload::EmbassyExecutorPollStart {
+            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+        };
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_executor_idle(executor_id: u32) {
-    let payload = EventPayload::EmbassyExecutorIdle {
-        executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-    };
+    critical_section::with(|_| {
+        let payload = EventPayload::EmbassyExecutorIdle {
+            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+        };
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_new(executor_id: u32, task_id: u32) {
-    let payload = EventPayload::TypeDefinition(TypeDefinitionPayload::EmbassyTaskCreated {
-        task_id: task_id,
-        executor_id_long: executor_id,
-        executor_id_short: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-    });
+    critical_section::with(|_| {
+        let payload = EventPayload::TypeDefinition(TypeDefinitionPayload::EmbassyTaskCreated {
+            task_id: task_id,
+            executor_id_long: executor_id,
+            executor_id_short: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+        });
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_end(executor_id: u32, task_id: u32) {
-    let payload = EventPayload::TypeDefinition(TypeDefinitionPayload::EmbassyTaskEnded {
-        task_id: task_id,
-        executor_id_long: executor_id,
-        executor_id_short: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-    });
+    critical_section::with(|_| {
+        let payload = EventPayload::TypeDefinition(TypeDefinitionPayload::EmbassyTaskEnded {
+            task_id: task_id,
+            executor_id_long: executor_id,
+            executor_id_short: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+        });
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_exec_begin(executor_id: u32, task_id: u32) {
-    let core_id = get_current_core_id();
+    critical_section::with(|_| {
+        let core_id = get_current_core_id();
 
-    let payload = match core_id {
-        0 => EventPayload::EmbassyTaskExecBeginCore0 {
-            task_id: compressed_task_id(task_id),
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        },
-        1 => EventPayload::EmbassyTaskExecBeginCore1 {
-            task_id: compressed_task_id(task_id),
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        },
-        c => unreachable_core_id(c),
-    };
+        let payload = match core_id {
+            0 => EventPayload::EmbassyTaskExecBeginCore0 {
+                task_id: compressed_task_id(task_id),
+                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+            },
+            1 => EventPayload::EmbassyTaskExecBeginCore1 {
+                task_id: compressed_task_id(task_id),
+                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+            },
+            c => unreachable_core_id(c),
+        };
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_exec_end(executor_id: u32, _task_id: u32) {
-    let core_id = get_current_core_id();
+    critical_section::with(|_| {
+        let core_id = get_current_core_id();
 
-    let payload = match core_id {
-        0 => EventPayload::EmbassyTaskExecEndCore0 {
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        },
-        1 => EventPayload::EmbassyTaskExecEndCore1 {
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        },
-        c => unreachable_core_id(c),
-    };
+        let payload = match core_id {
+            0 => EventPayload::EmbassyTaskExecEndCore0 {
+                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+            },
+            1 => EventPayload::EmbassyTaskExecEndCore1 {
+                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+            },
+            c => unreachable_core_id(c),
+        };
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_ready_begin(executor_id: u32, task_id: u32) {
-    let payload = EventPayload::EmbassyTaskReady {
-        task_id: compressed_task_id(task_id),
-        executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-    };
+    critical_section::with(|_| {
+        let payload = EventPayload::EmbassyTaskReady {
+            task_id: compressed_task_id(task_id),
+            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
+        };
 
-    write_tracing_event(payload);
+        write_tracing_event(payload);
+    });
 }
