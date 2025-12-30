937c7b23a0a3405ab7f023ffbddc223125b746d4
Christopher Kock <christopher-kock@outlook.de>
2025-12-30 09:52:33 +0100

    Remove unused

--- rustmeter-cli/src/logs/defmt_line.rs
+++ rustmeter-cli/src/logs/defmt_line.rs
@@ -34,37 +34,6 @@ impl DefmtLine {
     pub fn message(&self) -> &str {
         &self.message
     }
-
-    /// Parse a log line from a string: e.q. "0.438284 [DEBUG ] pop - New prio level: 0 (esp_rtos esp-rtos-0.2.0/src/run_queue.rs:292)"
-    pub fn try_from_str(line: &str) -> anyhow::Result<DefmtLine> {
-        // Find open / close brackets for log level
-        let open_bracket = line.find('[').ok_or(anyhow::anyhow!(
-            "Invalid log line format (found no opening bracket): {line}"
-        ))?;
-        let close_bracket = line.find(']').ok_or(anyhow::anyhow!(
-            "Invalid log line format (found no closing bracket): {line}"
-        ))?;
-        if open_bracket > close_bracket.saturating_sub(3) {
-            return Err(anyhow::anyhow!(
-                "Invalid log line format (malformed log level brackets): {line}"
-            ));
-        }
-
-        // Extract parts
-        let timestamp_str = &line[0..open_bracket].trim();
-        let level_str = &line[open_bracket + 1..close_bracket].trim();
-        let message = line[close_bracket + 1..].trim().to_string();
-
-        // Parse
-        let timestamp = match timestamp_str.parse::<f64>() {
-            Ok(val) => Some(Duration::from_secs_f64(val).as_micros() as u64),
-            Err(_) => None,
-        };
-        let level = DefmtLogLevel::try_from_str(level_str)
-            .context("Failed to parse log level of log line")?;
-
-        Ok(DefmtLine::new(level, timestamp, message))
-    }
 }
 
 impl Display for DefmtLine {
