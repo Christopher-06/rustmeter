9c2656011957dad4713e0294962bfc1deaea4581
Christopher Kock <christopher-kock@outlook.de>
2026-01-07 17:53:14 +0100

    Define raw-writers to reduce event overhead

---
+++ rustmeter-beacon/rustmeter-beacon-core/src/protocol/raw_writers.rs
@@ -0,0 +1,284 @@
+use crate::{time_delta::TimeDelta, tracing::write_tracing_data};
+use arbitrary_int::{traits::Integer, u3};
+
+pub mod event_ids {
+    pub const EMBASSY_TASK_READY: u8 = 1;
+    pub const EMBASSY_TASK_EXEC_BEGIN_CORE0: u8 = 2;
+    pub const EMBASSY_TASK_EXEC_BEGIN_CORE1: u8 = 3;
+    pub const EMBASSY_TASK_EXEC_END_CORE0: u8 = 4;
+    pub const EMBASSY_TASK_EXEC_END_CORE1: u8 = 5;
+    pub const EMBASSY_EXECUTOR_POLL_START: u8 = 6;
+    pub const EMBASSY_EXECUTOR_IDLE: u8 = 7;
+    pub const MONITOR_START_CORE0: u8 = 8;
+    pub const MONITOR_START_CORE1: u8 = 9;
+    pub const MONITOR_END_CORE0: u8 = 10;
+    pub const MONITOR_END_CORE1: u8 = 11;
+    pub const MONITOR_VALUE: u8 = 12;
+    pub const TYPE_DEFINITION: u8 = 13;
+    pub const DATA_LOSS_EVENT: u8 = 14;
+}
+
+#[inline(always)]
+pub fn write_embassy_task_ready(task_id: u16, executor_id: u3) {
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = (event_ids::EMBASSY_TASK_READY << 3) | executor_id.as_u8();
+    buffer[1..3].copy_from_slice(&task_id.to_le_bytes());
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[3..]);
+    unsafe { write_tracing_data(&buffer[..3 + pos]) };
+}
+
+#[inline(always)]
+pub fn write_embassy_task_exec_begin(core_id: u8, task_id: u16, executor_id: u3) {
+    let event_id = match core_id {
+        0 => event_ids::EMBASSY_TASK_EXEC_BEGIN_CORE0 << 3,
+        1 => event_ids::EMBASSY_TASK_EXEC_BEGIN_CORE1 << 3,
+        _ => return, // Invalid core ID
+    };
+
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = event_id | executor_id.as_u8();
+    buffer[1..3].copy_from_slice(&task_id.to_le_bytes());
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[3..]);
+    unsafe { write_tracing_data(&buffer[..3 + pos]) };
+}
+
+#[inline(always)]
+pub fn write_embassy_task_exec_end(core_id: u8, executor_id: u3) {
+    let event_id = match core_id {
+        0 => event_ids::EMBASSY_TASK_EXEC_END_CORE0 << 3,
+        1 => event_ids::EMBASSY_TASK_EXEC_END_CORE1 << 3,
+        _ => return, // Invalid core ID
+    };
+
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = event_id | executor_id.as_u8();
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[1..]);
+    unsafe { write_tracing_data(&buffer[..1 + pos]) };
+}
+
+#[inline(always)]
+pub fn write_embassy_executor_poll_start(executor_id: u3) {
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = (event_ids::EMBASSY_EXECUTOR_POLL_START << 3) | executor_id.as_u8();
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[1..]);
+    unsafe { write_tracing_data(&buffer[..1 + pos]) };
+}
+
+#[inline(always)]
+pub fn write_embassy_executor_idle(executor_id: u3) {
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = (event_ids::EMBASSY_EXECUTOR_IDLE << 3) | executor_id.as_u8();
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[1..]);
+    unsafe { write_tracing_data(&buffer[..1 + pos]) };
+}
+
+#[inline(always)]
+pub fn write_monitor_start(core_id: u8, monitor_id: u8) {
+    let event_id = match core_id {
+        0 => event_ids::MONITOR_START_CORE0 << 3,
+        1 => event_ids::MONITOR_START_CORE1 << 3,
+        _ => return, // Invalid core ID
+    };
+
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = event_id;
+    buffer[1] = monitor_id;
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[2..]);
+    unsafe { write_tracing_data(&buffer[..2 + pos]) };
+}
+
+#[inline(always)]
+pub fn write_monitor_end(core_id: u8) {
+    let event_id = match core_id {
+        0 => event_ids::MONITOR_END_CORE0 << 3,
+        1 => event_ids::MONITOR_END_CORE1 << 3,
+        _ => return, // Invalid core ID
+    };
+
+    // Add payload
+    let mut buffer = [0u8; 8];
+    buffer[0] = event_id;
+
+    // Write to global buffer
+    let timestamp = TimeDelta::from_now();
+    let pos = timestamp.write_bytes_mut(&mut buffer[1..]);
+    unsafe { write_tracing_data(&buffer[..1 + pos]) };
+}
+
+// TODO: Implement monitor value!
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use crate::{
+        buffer::BufferReader, mocks::test_mocks::with_mocks, protocol::EventPayload,
+        tracing::read_tracing_event,
+    };
+    use arbitrary_int::u3;
+
+    fn mock_time_provider() -> u32 {
+        123_456_789
+    }
+
+    // Mock trace writer that checks the written data to match the expected event payload
+    fn mock_trace_writer(expected: EventPayload) -> impl Fn(&[u8]) {
+        move |data: &[u8]| {
+            let mut buffer = BufferReader::new(data);
+            let (timestamp, event) =
+                read_tracing_event(&mut buffer, &|_| None).expect("Failed to read tracing event");
+
+            assert_eq!(timestamp.get_delta_us(), 123_456_789);
+            assert_eq!(event, expected);
+        }
+    }
+
+    #[test]
+    fn test_write_embassy_task_ready() {
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyTaskReady {
+                task_id: 12345,
+                executor_id: u3::new(5),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_task_ready(12345, u3::new(5));
+            },
+        );
+    }
+
+    #[test]
+    fn test_write_embassy_task_exec_begin() {
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyTaskExecBeginCore0 {
+                task_id: 54321,
+                executor_id: u3::new(2),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_task_exec_begin(0, 54321, u3::new(2));
+            },
+        );
+
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyTaskExecBeginCore1 {
+                task_id: 54321,
+                executor_id: u3::new(2),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_task_exec_begin(1, 54321, u3::new(2));
+            },
+        );
+    }
+
+    #[test]
+    fn test_write_embassy_task_exec_end() {
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyTaskExecEndCore0 {
+                executor_id: u3::new(3),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_task_exec_end(0, u3::new(3));
+            },
+        );
+
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyTaskExecEndCore1 {
+                executor_id: u3::new(3),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_task_exec_end(1, u3::new(3));
+            },
+        );
+    }
+
+    #[test]
+    fn test_write_embassy_executor_poll_start() {
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyExecutorPollStart {
+                executor_id: u3::new(1),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_executor_poll_start(u3::new(1));
+            },
+        );
+    }
+
+    #[test]
+    fn test_write_embassy_executor_idle() {
+        with_mocks(
+            mock_trace_writer(EventPayload::EmbassyExecutorIdle {
+                executor_id: u3::new(4),
+            }),
+            mock_time_provider,
+            || {
+                write_embassy_executor_idle(u3::new(4));
+            },
+        );
+    }
+
+    #[test]
+    pub fn test_write_monitor_start() {
+        with_mocks(
+            mock_trace_writer(EventPayload::MonitorStartCore0 { monitor_id: 10 }),
+            mock_time_provider,
+            || {
+                write_monitor_start(0, 10);
+            },
+        );
+
+        with_mocks(
+            mock_trace_writer(EventPayload::MonitorStartCore1 { monitor_id: 20 }),
+            mock_time_provider,
+            || {
+                write_monitor_start(1, 20);
+            },
+        );
+    }
+
+    #[test]
+    pub fn test_write_monitor_end() {
+        with_mocks(
+            mock_trace_writer(EventPayload::MonitorEndCore0),
+            mock_time_provider,
+            || {
+                write_monitor_end(0);
+            },
+        );
+
+        with_mocks(
+            mock_trace_writer(EventPayload::MonitorEndCore1),
+            mock_time_provider,
+            || {
+                write_monitor_end(1);
+            },
+        );
+    }
+}
--- rustmeter-beacon/rustmeter-beacon-target/src/embassy_trace.rs
+++ rustmeter-beacon/rustmeter-beacon-target/src/embassy_trace.rs
@@ -3,13 +3,20 @@
 
 use rustmeter_beacon_core::{
     compressed_task_id,
-    protocol::{EventPayload, TypeDefinitionPayload},
+    protocol::{
+        EventPayload, TypeDefinitionPayload,
+        raw_writers::{
+            write_embassy_executor_idle, write_embassy_executor_poll_start,
+            write_embassy_task_exec_begin, write_embassy_task_exec_end, write_embassy_task_ready,
+        },
+    },
     tracing::write_tracing_event,
 };
+use arbitrary_int::traits::Integer;
 
 use crate::{
     core_id::{get_current_core_id, unreachable_core_id},
-    executor_registry::ExecutorRegistry,
+    executor_registry::ExecutorRegistry, get_tracing_time_us,
 };
 
 // Registry to map long executor IDs to short IDs
@@ -17,24 +24,16 @@ static EXECUTOR_REGISTRY: ExecutorRegistry = ExecutorRegistry::new();
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_poll_start(executor_id: u32) {
-    critical_section::with(|_| {
-        let payload = EventPayload::EmbassyExecutorPollStart {
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        };
+    let executor_id = EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap();
 
-        write_tracing_event(payload);
-    });
+    write_embassy_executor_poll_start(executor_id);
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_executor_idle(executor_id: u32) {
-    critical_section::with(|_| {
-        let payload = EventPayload::EmbassyExecutorIdle {
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        };
+    let executor_id = EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap();
 
-        write_tracing_event(payload);
-    });
+    write_embassy_executor_idle(executor_id);
 }
 
 #[unsafe(no_mangle)]
@@ -65,52 +64,33 @@ fn _embassy_trace_task_end(executor_id: u32, task_id: u32) {
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_exec_begin(executor_id: u32, task_id: u32) {
-    critical_section::with(|_| {
-        let core_id = get_current_core_id();
+    let core_id = get_current_core_id();
+    let executor_id = EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap();
 
-        let payload = match core_id {
-            0 => EventPayload::EmbassyTaskExecBeginCore0 {
-                task_id: compressed_task_id(task_id),
-                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-            },
-            1 => EventPayload::EmbassyTaskExecBeginCore1 {
-                task_id: compressed_task_id(task_id),
-                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-            },
-            c => unreachable_core_id(c),
-        };
-
-        write_tracing_event(payload);
-    });
+    write_embassy_task_exec_begin(core_id, compressed_task_id(task_id), executor_id);
 }
 
 #[unsafe(no_mangle)]
 fn _embassy_trace_task_exec_end(executor_id: u32, _task_id: u32) {
-    critical_section::with(|_| {
-        let core_id = get_current_core_id();
+    let core_id = get_current_core_id();
+    let executor_id = EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap();
 
-        let payload = match core_id {
-            0 => EventPayload::EmbassyTaskExecEndCore0 {
-                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-            },
-            1 => EventPayload::EmbassyTaskExecEndCore1 {
-                executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-            },
-            c => unreachable_core_id(c),
-        };
-
-        write_tracing_event(payload);
-    });
+    write_embassy_task_exec_end(core_id, executor_id);
 }
 
 #[unsafe(no_mangle)]
+#[esp_hal::ram]
 fn _embassy_trace_task_ready_begin(executor_id: u32, task_id: u32) {
-    critical_section::with(|_| {
-        let payload = EventPayload::EmbassyTaskReady {
-            task_id: compressed_task_id(task_id),
-            executor_id: EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap(),
-        };
+    let executor_id = EXECUTOR_REGISTRY.lookup_or_register(executor_id).unwrap();
+    let start_time = get_tracing_time_us();
 
-        write_tracing_event(payload);
-    });
+    write_embassy_task_ready(compressed_task_id(task_id), executor_id);
+
+    let end_time = get_tracing_time_us();
+    // defmt::info!(
+    //     "Task ready begin: task_id={}, executor_id={}, time_taken={}us",
+    //     task_id,
+    //     executor_id.as_u8(),
+    //     (end_time - start_time) as f32 / 240.0
+    // );
 }
